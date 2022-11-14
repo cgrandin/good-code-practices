a38817351fb75428f6a4ca3a9e668c27b1119b9b
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2022-11-14 09:05:07 -0800

    Add rbind question and tidied up version.

---
+++ R/rbind-question-tidy.R
@@ -0,0 +1,22 @@
+# Question from Catarina:
+#  I wanted to extract and rbind all the “um” data frames in this nested list.
+# First, improve formatting of code.
+#  in Emacs)
+library(parallel)
+
+result <- mclapply(mc.cores = ncores,
+                   X = 1:5,
+                   FUN = function(a){
+                     out <- mclapply(mc.cores = ncores,
+                                     X = 1:20,
+                                     FUN = function(u){
+                                       um <- data.frame(oa = paste0("o", 1:a),
+                                                        a=a)
+                                       dois <- data.frame(dois = paste0("i", u))
+                                       return(structure(list(um=um,
+                                                             dois=dois)))
+                                     }
+                                     )
+                     return(list(out))
+                   }
+)
