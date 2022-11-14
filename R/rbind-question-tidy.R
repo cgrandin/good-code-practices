395d27478baf1fb8aa3d9407e505caf8b9a4751d
Chris Grandin <chris.grandin@gmail.com>
2022-11-14 09:25:42 -0800

    Add simpler code for Catarina's question * Works on Windows and Linux/MAC * Uses map()_

--- R/rbind-question-tidy.R
+++ R/rbind-question-tidy.R
@@ -4,19 +4,31 @@
 #  in Emacs)
 library(parallel)
 
-result <- mclapply(mc.cores = ncores,
+result <- mclapply(mc.cores = 1,
                    X = 1:5,
                    FUN = function(a){
-                     out <- mclapply(mc.cores = ncores,
+                     out <- mclapply(mc.cores = 1,
                                      X = 1:20,
                                      FUN = function(u){
                                        um <- data.frame(oa = paste0("o", 1:a),
-                                                        a=a)
+                                                        a = a)
                                        dois <- data.frame(dois = paste0("i", u))
-                                       return(structure(list(um=um,
-                                                             dois=dois)))
-                                     }
-                                     )
-                     return(list(out))
-                   }
-)
+                                       structure(list(um = um,
+                                                      dois = dois))
+                                     })
+                     list(out)
+                   })
+
+# Same code simplified, using purrr::map()
+result_map <- map(1:5,
+                  function(a){
+                    out <- map(1:20,
+                               function(u){
+                                 um <- data.frame(oa = paste0("o", 1:a),
+                                                  a = a)
+                                 dois <- data.frame(dois = paste0("i", u))
+                                 structure(list(um = um,
+                                                dois = dois))
+                               })
+                    list(out)
+                  })
